934852ea11afce71516cb9d4b4b7ddc63ee2c5fb
Никита Осипцов <osipczovnikita@gmail.com>
2021-10-24 15:46:10 +0300

    visual fixes

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,11 +1,11 @@
 #include <conio.h>
 #include <dos.h>
 
-void scroll(int direction, int l_row, int l_col, int r_row, int r_col, int attr)
+void scroll(int direction, int step, int l_row, int l_col, int r_row, int r_col, int attr)
 {
 	union REGS r;
 	r.h.ah = 6 + direction;
-	r.h.al = 1;
+	r.h.al = step;
 	r.h.ch = l_row; r.h.cl = l_col; r.h.dh = r_row; r.h.dl = r_col;
 	r.h.bh = attr;
 	int86(0x10, &r, &r);
@@ -19,8 +19,8 @@ int main()
 	const int downRightX = 70;
 	const int downRightY = 20;
 	const int stringStep = 3;
-	const int maxPhraseLen = 26;
-	const float timeStep = 1.3;
+	const int maxPhraseLen = 21;
+	const int timeStep = 1300;
 	const char* colNames[] = { "BLACK", "BLUE", "GREEN", "CYAN", "RED", "MAGENTA", "BROWN", "LIGHTGRAY", "DARKGRAY", "LIGHTBLUE", "LIGHTGREEN", "LIGHTCYAN", "LIGHTRED", "LIGHTMAGENTA", "YELLOW", "WHITE" };
 	enum DIRECTIONS { DOWN, UP };
 
@@ -34,8 +34,8 @@ int main()
 	clrscr();
 
 	gotoxy(1, downRightY - upLeftY + 1);
-	for (int txtColor = BLACK; txtColor <= WHITE; txtColor++)
-		for (int bgColor = BLACK; bgColor <= WHITE; bgColor++)
+	for (int bgColor = BLACK; bgColor <= LIGHTGRAY; bgColor++)
+		for (int txtColor = BLACK; txtColor <= WHITE; txtColor++)
 		{
 			textcolor(txtColor);
 			textbackground(bgColor);
@@ -43,14 +43,14 @@ int main()
 
 			textcolor(BLACK);
 			textbackground(LIGHTGRAY);
-			cprintf("    %s    %d", colNames[bgColor], txtColor);
-			delay((int)(1000 * timeStep));
+			cprintf("   Background: %-12s Text: %2d", colNames[bgColor], txtColor);
+			delay(timeStep);
+
 			gotoxy(1, wherey());
-
-			for (int i = wherey() - 1; i < stringStep + 1; i++) scroll(UP, upLeftY - 1, upLeftX - 1, downRightY - 1, downRightX - 1, 0x70);
-
+			if (wherey() < stringStep)
+				scroll(UP, stringStep, upLeftY - 1, upLeftX - 1, downRightY - 1, downRightX - 1, 0x70);
 			gotoxy(1, wherey() - stringStep);
 		}
-
+	getch();
 	return 0;
 }
